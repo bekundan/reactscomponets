3d1f969a09fbd5687892141bbd1ee73db82605b0
kundan gupta <56871880+bekundan@users.noreply.github.com>
2020-11-15 19:52:05 +0530

    here we can import another way.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { add, multiply, subtract, divide } from "./calculator";
+// import { add, multiply, subtract, divide } from "./calculator";
+//also we can imports like this
+import * as Calculator from "./calculator";
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
 //If successful, your website should look the same as the Final.png
 
 ReactDOM.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
+    <li>{Calculator.add(1, 2)}</li>
+    <li>{Calculator.multiply(2, 3)}</li>
+    <li>{Calculator.subtract(7, 2)}</li>
+    <li>{Calculator.divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
